f5d79dd8f17b3f19f78d6f6e5b14006f99072150
godwinagamah <gagamah@byupathway.edu>
2026-05-26 21:37:04 +0000

    the project is good

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        
+        
+        
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        Console.WriteLine("I am thinking of a number between 1 and 100...");
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
