6edd60aa810d819909a671889dd11eb848d4b136
godwinagamah <gagamah@byupathway.edu>
2026-05-26 21:23:50 +0000

    making necessary changes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage: ");
+        double grade = Convert.ToDouble(Console.ReadLine());
+        string letter = "";
+
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        Console.WriteLine(new string('-', 30)); 
+
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course! Keep up the great work.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Work hard, ask for help, and you'll get it next time.");
+        }
     }
 }
+    
