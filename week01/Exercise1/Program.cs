ec90e526b69b0b6d9b2179e2690ec4d9f21b19d8
godwinagamah <gagamah@byupathway.edu>
2026-05-26 19:17:58 +0000

    made a change in the first

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your fist name? ");
+        Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
