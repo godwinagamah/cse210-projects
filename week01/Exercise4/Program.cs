da0e70e3454fa6fa4533d4ac984bbaa7a40b0844
godwinagamah <gagamah@byupathway.edu>
2026-05-26 21:55:20 +0000

    More room for improvement

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic; 
+using System.Linq;            
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        
+        int userNumber = -1; 
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+
+        
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+    
+        if (numbers.Count > 0)
+        {
+            
+            int sum = numbers.Sum();
+
+            
+            double average = numbers.Average();
+
+            
+            int max = numbers.Max();
+
+            
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
